5cd0dad8c3eb10167cefc86832e592567c54d6ef
Mitch Long <meechllada@gmail.com>
2018-02-17 10:57:24 -0800

    create boundry

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 let x = 50;
-let y = 50;
+let y = 200;
+let dim = {height: 500, width:500};
 
 function setup() {
-
+  createCanvas(dim.width, dim.height);
 }
 
 function draw() {
+  rect(0,0, dim.width-1, dim.height-1)
   let w = 50;
   let h = 100;
   triangle( 
