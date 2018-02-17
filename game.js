f42c70a6b20a294155ebbc5e53cbdbd507a9f9ce
Mitch Long <meechllada@gmail.com>
2018-02-17 11:13:43 -0800

    refactor space

--- game.js
+++ game.js
@@ -1,22 +1,26 @@
 let ship = {
     pos: { x: 50, y: 200 },
-    dim: { w: 50, h: 100 }
+    space: { w: 50, h: 100 }
 };
+
 function drawShip(ship){
   triangle( 
-    ship.pos.x-ship.dim.w/2, ship.pos.y+ship.dim.h/2, 
-    ship.pos.x+ship.dim.w/2, ship.pos.y+ship.dim.h/2,
-    ship.pos.x,             ship.pos.y - ship.dim.h/2);
+    ship.pos.x-ship.space.w/2, ship.pos.y+ship.space.h/2, 
+    ship.pos.x+ship.space.w/2, ship.pos.y+ship.space.h/2,
+    ship.pos.x,             ship.pos.y - ship.space.h/2);
 }
 
-let dim = {height: 500, width:500};
+function drawSpaceBoundary(space) {
+  rect(0,0, space.width-1, space.height-1);
+}
+
+let space = {height: 500, width:500};
 
 function setup() {
-  createCanvas(dim.width, dim.height);
+  createCanvas(space.width, space.height);
 }
 
 function draw() {
-  rect(0,0, dim.width-1, dim.height-1)
-
+  drawSpaceBoundary(space);
   drawShip(ship);
 }
